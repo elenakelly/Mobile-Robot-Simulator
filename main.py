0f607d7f46cd19700582c053947f0ecb205eef86
Elena Kane <59971317+elenakelly@users.noreply.github.com>
2022-02-09 10:51:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from turtle import Screen, update
+from turtle import Screen, distance, update
 from numpy import disp
 import random
 import pygame 
+import math
 
 
 pygame.init()
 
 #create screen 
 display = pygame.display.set_mode((800,600))
-pygame.display.update()
+background = pygame.image.load('background.png')
 
 pygame.display.set_caption("Mobile Robot Simulator")
 icon = pygame.image.load('icon.png')
@@ -17,14 +18,43 @@ pygame.display.set_icon(icon)
 
 #robot
 robotimg = pygame.image.load('vacuum.png')
-robotx = random.uniform(0,400)
-roboty = random.uniform(0,400)
+robotx = random.uniform(0,735)
+roboty = random.uniform(0,535)
 
 robotx_change = 0
 roboty_change = 0
 
+#wall
+
+wallimg = []
+wallx = []
+wally = []
+wallx_change = []
+wally_change = []
+num_walls = 10
+
+for i in range(num_walls):
+
+    wallimg.append(pygame.image.load('wall.png'))
+    wallx.append(random.uniform(0,800))
+    wally.append(random.uniform(0,600))
+
+    
+    
+
+
+
 def robot (x,y):
     display.blit(robotimg, (x,y))
+
+def wall (x,y,i):
+    display.blit(wallimg[i], (x,y))
+
+def isCollision(robotx,roboty,wallx,wally):
+    distance =  math.sqrt(math.pow(robotx-roboty,2)+ (math.pow(wallx-wally,2)))
+    if distance <27:
+        return True 
+    
 
 
 #game loop
@@ -34,6 +64,9 @@ while open:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             open = False
+
+
+        display.blit(background,(0,0))
 
         if event.type == pygame.KEYDOWN:
             print("press")
@@ -55,14 +88,34 @@ while open:
                 print("release")
                 robotx_change = 0
                 roboty_change = 0
+        
+        for i in range(num_walls):
+        #Collision
+            collision = isCollision(robotx,roboty,wallx[i],wally[i])
+            if collision:
+                robotx -= 1
+                roboty -= 1
+            wall(wallx[i],wally[i],i)
     
-    
-    display.fill((255,255,255))
 
     robotx += robotx_change
     roboty += roboty_change
+
+    if robotx <= 0:
+        robotx = 0
+    elif robotx >= 736:
+        robotx = 736
+
+    if roboty <= 0:
+        roboty = 0
+    elif roboty >= 536:
+        roboty = 536
+
     robot(robotx,roboty)
+    
+
+
     pygame.display.update()        
 
 pygame.quit()
-quit()            
+quit()                       
